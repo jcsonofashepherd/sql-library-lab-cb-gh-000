97a27eace409d966af45cd45c0c84a235d0472e6
John Casabar <johncasabar@gmail.com>
2018-10-22 01:37:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,5 +28,5 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books
   ON characters.id = character_books.character_id
-  GROUP BY characters.name"
+  GROUP BY character_books.character_id"
 end
