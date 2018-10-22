085bbd3bf13f60f32b2f57cd4342324c04d57c8b
John Casabar <johncasabar@gmail.com>
2018-10-22 02:16:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,9 +18,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN series
-  ON authors.id = series.author_id
-  WHERE series.subgenre_id = subgenres.id"
+  INNER JOIN series, subgenres
+  ON authors.id = series.author_id, subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
