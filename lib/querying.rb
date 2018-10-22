1442553c46b4c7e2f9b517891a9644d5433786f9
John Casabar <johncasabar@gmail.com>
2018-10-22 01:52:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,13 +20,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT series.title, COUNT(characters.species)
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
   GROUP BY characters.species
-  ORDER BY COUNT(characters.species) DESC
-  LIMIT 1"
+  ORDER BY COUNT(characters.species) DESC"
 end
 
 def select_character_names_and_number_of_books_they_are_in
