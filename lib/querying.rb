2c841ad3595f2e8a3804b82434beda8efe333587
John Casabar <johncasabar@gmail.com>
2018-10-22 01:52:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
-  GROUP BY species
+  GROUP BY characters.species
   ORDER BY COUNT(species) DESC
   LIMIT 1"
 end
